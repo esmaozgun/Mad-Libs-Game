8bf2366936d740d97eb79c02f5048a3cf72059ff
esmaozgun <esma.ozgun@kobil.com>
2024-02-22 16:28:53 +0300

    first commit

---
+++ Madlibs.py
@@ -0,0 +1,9 @@
+noun = input("Noun: ")
+verb = input("Verb: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+verb2 = input("Verb: ")
+
+madlibs = f"Hey! Welcome to my {noun}. Here, we {verb} about {noun2} and we have fun with {noun3}. Make sure you bring {verb2} to my {noun} "
+
+print (madlibs)
