c5f3ee08332f17f8a988001cfbc7e8b415a1c1f6
Esma Özgün <nuresma349@gmail.com>
2024-04-29 20:00:02 +0300

    Update Madlibs.py

--- Madlibs.py
+++ Madlibs.py
@@ -1,9 +1,23 @@
-noun = input("Noun: ")
-verb = input("Verb: ")
-noun2 = input("Noun: ")
-noun3 = input("Noun: ")
-verb2 = input("Verb: ")
+def madlibs():
+    print("Welcome to the Madlibs game!")
+    print("Please fill in the blanks in the story by answering the following questions.\n")
 
-madlibs = f"Hey! Welcome to my {noun}. Here, we {verb} about {noun2} and we have fun with {noun3}. Make sure you bring {verb2} to my {noun} "
+    name = input("Enter a name: ")
+    verb_past = input("Enter a verb (past tense): ")
+    adjective = input("Enter an adjective: ")
+    noun_plural = input("Enter a plural noun: ")
+    place = input("Enter a place: ")
+    animal = input("Enter an animal: ")
+    food = input("Enter a food: ")
+    emotion = input("Enter an emotion: ")
 
-print (madlibs)
+    story = f"""
+    One day, {name} was walking in {place} when they saw a {animal}. 
+    The {animal} looked at {name} and said "{emotion}!". 
+    Surprised, {name} {verb_past} away quickly. 
+    Later, they realized they had dropped their {noun_plural} behind. 
+    The {animal} was actually trying to return it, along with a piece of {food}. 
+    {name} felt {adjective} and thanked the {animal} for its kindness.
+    """
+    
+    madlibs()
